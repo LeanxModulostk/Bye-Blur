29751648c436ca8d2bf6565f60c68a7efe354be2
@LeanHijosdesusMadres <105339980+LeanxModulostk@users.noreply.github.com>
2024-05-23 14:24:10 -0500

    Add files via upload

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -7,9 +7,13 @@ resetprop -n ro.sf.blurs_are_expensive 0
 resetprop -n persist.sys.sf.disable_blurs true
 resetprop -n enable_blurs_on_windows 0
 resetprop -n disableBlurs true
+resetprop -n disableBackgroundBlur true
 resetprop -n ro.sf.blurs_are_caro 1
 resetprop -n ro.miui.has_real_blur 0
 resetprop -n persist.sys.background_blur_supported false
+
+su -c cmd window disable-blur 1
+su -c wm disable-blur 1
 
 #gracias a @nonosvaimos
 #t.me/modulostk
---
+++ service.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+# @modulostk [Telegram]
+sleep 40
+
+su -c cmd window disable-blur 1
+su -c wm disable-blur 1
+
+exit 0
